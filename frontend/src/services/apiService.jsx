c58c71748a12019a89c4933b403753e1fad6b153
Sunil Kumar Sahoo <sonu8847838585@gmail.com>
2024-12-07 13:06:18 +0530

    making it deployment ready

--- frontend/src/services/apiService.jsx
+++ frontend/src/services/apiService.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://persona-chat-bot-backend1.vercel.app/api',
+    // baseURL: 'http://localhost:5000/api',
     // baseURL: `${process.env.BACKEND_URL}/api`,
     headers: {
         'Content-Type': 'application/json',
