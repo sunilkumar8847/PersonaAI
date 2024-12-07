f2c25e5b10b33355e90b21b34e8b1716026c870e
Sunil Kumar Sahoo <sonu8847838585@gmail.com>
2024-12-07 11:58:39 +0530

    ready for deployment4

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const twitterRoutes = require('./routes/twitterRoutes');
 const app = express();
 
 app.use(cors({
-  origin: `http://localhost:5173`,
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 }));
