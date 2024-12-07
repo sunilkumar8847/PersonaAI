a93fd7f248cca25e7f5e0e902a35c02cef5c1d32
Sunil Kumar Sahoo <sonu8847838585@gmail.com>
2024-12-07 11:48:20 +0530

    ready for deployment3

--- frontend/src/services/apiService.jsx
+++ frontend/src/services/apiService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: `${import.meta.env.BACKEND_URL}/api`,
+    baseURL: `${process.env.BACKEND_URL}/api`,
     headers: {
         'Content-Type': 'application/json',
     },
