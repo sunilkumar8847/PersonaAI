2f3f62a732834c95ca9d4da12090f30e10a611f8
Sunil Kumar Sahoo <sonu8847838585@gmail.com>
2024-12-14 12:03:04 +0530

    backend API changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar({ message }) {
       <nav className="container mx-auto flex items-center justify-center p-4">
         <h2 className="text-xl font-bold">
           <span>Chat with </span>
-          <span className='text-blue-500 '>@{message}</span>
+          <span className='text-blue-500 '>{message}</span>
         </h2>
       </nav>
     </header>
